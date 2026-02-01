aed1763a3f00a93a3ea960173cf9e2cdde99dd4b
GitHub Action <action@github.com>
2026-02-01 18:03:41 +0100

    🔖 Bump version to 1.47.10 [skip ci]

--- src/const.ts
+++ src/const.ts
@@ -1,4 +1,4 @@
-export const CARD_VERSION = '1.47.8';
+export const CARD_VERSION = '1.47.10';
 
 export const CARD_NAME = 'ha-room-card';
 // Use valid custom element name (no colon) for browser registration
