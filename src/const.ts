980b1ea75cbab8ed137e9142609b1b1d5473251a
GitHub Action <action@github.com>
2026-02-01 15:56:30 +0100

    🔖 Release v1.47.6

--- src/const.ts
+++ src/const.ts
@@ -1,4 +1,4 @@
-export const CARD_VERSION = '1.47.5';
+export const CARD_VERSION = '1.47.6';
 
 export const CARD_NAME = 'ha-room-card';
 // Use valid custom element name (no colon) for browser registration
